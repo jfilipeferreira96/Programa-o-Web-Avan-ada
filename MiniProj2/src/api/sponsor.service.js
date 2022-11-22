3f768aeaa5cbb4c76f19bb757b790efe20e6d3d9
Filipe Ferreira <filipeferreira96@hotmail.com>
2022-11-22 21:27:53 +0000

    new changes to constants

---
+++ MiniProj2/src/api/sponsor.service.js
@@ -0,0 +1,86 @@
+import API_URL from "./config.js";
+
+export const sponorService = {
+  async getSponsors(token) {
+    const response = await fetch(`${API_URL}/sponsor`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: token,
+      },
+    });
+    if (response.ok) {
+      return await response.json();
+    } else {
+      throw Error(handleResponses(response.status));
+    }
+  },
+
+  async addSponsor(token, payload) {
+    const response = await fetch(`${API_URL}/sponsor`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: token,
+      },
+      body: JSON.stringify(payload),
+    });
+    if (response.ok) {
+      return await response.json();
+    } else {
+      throw Error(handleResponses(response.status));
+    }
+  },
+
+  async editSponsor(token, payload) {
+    const response = await fetch(`${API_URL}/sponsor/${payload._id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: token,
+      },
+      body: JSON.stringify(payload),
+    });
+    if (response.ok) {
+      return await response.json();
+    } else {
+      throw Error(handleResponses(response.status));
+    }
+  },
+
+  async removeSponsor(token, id) {
+    const response = await fetch(`${API_URL}/sponsor/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: token,
+      },
+    });
+    if (response.ok) {
+      return await response.json();
+    } else {
+      throw Error(handleResponses(response.status));
+    }
+  },
+};
+
+function handleResponses(code) {
+  let message = "";
+  switch (code) {
+    case 401:
+      message = "Não está autorizado a executar esta ação!";
+      break;
+    case 406:
+      message = "Ação não aceitável. Verifique se preencheu bem todos os campos!";
+      break;
+    case 409:
+      message = "Já existe um utilizador com este nome!";
+      break;
+    default:
+      message = "Mensagem desconhecida";
+      break;
+  }
+  return message;
+}
+
+export default sponorService;
