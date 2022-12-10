8d6dc8a7d60ebbdd664ce7d824fbf8aae61576ba
Filipe Ferreira <filipeferreira96@hotmail.com>
2022-12-10 21:23:26 +0000

    edit bug fix

--- MiniProj2/src/views/experts/EditExpert.vue
+++ MiniProj2/src/views/experts/EditExpert.vue
@@ -1,107 +1,75 @@
 <template>
   <section class="page-section">
-     <b-container>
-      <HeaderPage title="Editar Utilizador"/>
-   
+    <b-container>
+      <HeaderPage title="Editar Utilizador" />
+
       <!--FORM-->
       <b-row>
         <b-col cols="2"></b-col>
         <b-col cols="8">
           <form @submit.prevent="update">
             <div class="form-group">
-              <input
-                v-model="name"
-                type="text"
-                class="form-control form-control-lg"
-                id="txtName"
-                placeholder="escreve nome"
-                required
-              />
+              <input v-model="expert.name" type="text" class="form-control form-control-lg" id="txtName" placeholder="escreve nome" required />
             </div>
             <div class="form-group">
-               <input
-                v-model="contato"
-                type="number"
-                class="form-control form-control-lg"
-                id="txtContato"
-                placeholder="escreve contato telefonico"
-                maxlength="9"
-                required
-              />
+              <input v-model="expert.contato" type="number" class="form-control form-control-lg" id="txtContato" placeholder="escreve contato telefonico" maxlength="9" required />
             </div>
             <div class="form-group">
-               <input
-                v-model="especialidade"
-                type="text"
-                class="form-control form-control-lg"
-                id="txtEspecialidade"
-                placeholder="escreve a àrea de especialidade"
-                required
-              />
+              <input v-model="expert.especialidade" type="text" class="form-control form-control-lg" id="txtEspecialidade" placeholder="escreve a àrea de especialidade" required />
             </div>
             <div class="form-group">
-              <input
-                v-model="links[0].url"
-                type="url"
-                class="form-control form-control-lg"
-                id="txtPhoto"
-                placeholder="link da foto da especialidade"
-                required
-              />
+              <input v-model="expert.links[0].url" type="url" class="form-control form-control-lg" id="txtPhoto" placeholder="link da foto da especialidade" required />
             </div>
             <button type="submit" class="btn btn-outline-success btn-lg mr-2">
-              <i class="fas fa-edit"></i> ATUALIZAR</button>
-            <router-link
-              :to="{name: 'listExperts'}"
-              tag="button"
-              class="btn btn-outline-danger btn-lg"
-            ><i class="fas fa-window-close"></i> CANCELAR</router-link>
+              <i class="fas fa-edit"></i>
+              ATUALIZAR
+            </button>
+            <router-link :to="{ name: 'listExperts' }" tag="button" class="btn btn-outline-danger btn-lg">
+              <i class="fas fa-window-close"></i>
+              CANCELAR
+            </router-link>
           </form>
         </b-col>
         <b-col cols="2"></b-col>
       </b-row>
-     </b-container>
+    </b-container>
   </section>
 </template>
 
 <script>
 import { EDIT_EXPERT } from "@/store/experts/expert.constants";
-import HeaderPage from "@/components/HeaderPage.vue"
+import HeaderPage from "@/components/HeaderPage.vue";
 import router from "@/router";
 import { mapGetters } from "vuex";
 
 export default {
   name: "EditExpert",
   components: {
-    HeaderPage
+    HeaderPage,
   },
   data: () => {
     return {
-      expert: {}
+      expert: {},
     };
   },
   computed: {
-    ...mapGetters("expert", ["getExpertById", "getMessage"])
+    ...mapGetters("expert", ["getExpertById", "getMessage"]),
   },
   methods: {
     update() {
       this.$store.dispatch(`expert/${EDIT_EXPERT}`, this.$data.expert).then(
         () => {
-          this.$alert(
-            this.getMessage,
-            "Expert atualizado!",
-            "success"
-          );
-          router.push({name: 'listExperts'});
+          this.$alert(this.getMessage, "Expert atualizado!", "success");
+          router.push({ name: "listExperts" });
         },
-        err => {
+        (err) => {
           this.$alert(`${err.message}`, "Erro", "error");
         }
       );
-    }
+    },
   },
   created() {
     this.expert = this.getExpertById(this.$route.params.expertId);
-  }
+  },
 };
 </script>
--- MiniProj2/src/views/sponsors/EditSponsor.vue
+++ MiniProj2/src/views/sponsors/EditSponsor.vue
@@ -1,107 +1,75 @@
 <template>
   <section class="page-section">
-     <b-container>
-      <HeaderPage title="Editar Sponsor"/>
-   
+    <b-container>
+      <HeaderPage title="Editar Sponsor" />
+
       <!--FORM-->
       <b-row>
         <b-col cols="2"></b-col>
         <b-col cols="8">
           <form @submit.prevent="update">
             <div class="form-group">
-              <input
-                v-model="name"
-                type="text"
-                class="form-control form-control-lg"
-                id="txtName"
-                placeholder="escreve nome"
-                required
-              />
+              <input v-model="sponsor.name" type="text" class="form-control form-control-lg" id="txtName" placeholder="escreve nome" required />
             </div>
             <div class="form-group">
-               <input
-                v-model="contato"
-                type="number"
-                class="form-control form-control-lg"
-                id="txtContato"
-                placeholder="escreve contato telefonico"
-                maxlength="9"
-                required
-              />
+              <input v-model="sponsor.contato" type="number" class="form-control form-control-lg" id="txtContato" placeholder="escreve contato telefonico" maxlength="9" required />
             </div>
             <div class="form-group">
-               <input
-                v-model="animal"
-                type="text"
-                class="form-control form-control-lg"
-                id="txtAnimal"
-                placeholder="escreve o nome do animal patrocionado"
-                required
-              />
+              <input v-model="sponsor.animal" type="text" class="form-control form-control-lg" id="txtAnimal" placeholder="escreve o nome do animal patrocionado" required />
             </div>
             <div class="form-group">
-               <input
-                v-model="donation"
-                type="number"
-                class="form-control form-control-lg"
-                id="donation"
-                placeholder="valor em da doação €"
-                required
-              />
+              <input v-model="sponsor.donation" type="number" class="form-control form-control-lg" id="donation" placeholder="valor em da doação €" required />
             </div>
             <button type="submit" class="btn btn-outline-success btn-lg mr-2">
-              <i class="fas fa-edit"></i> ATUALIZAR</button>
-            <router-link
-              :to="{name: 'listSponsors'}"
-              tag="button"
-              class="btn btn-outline-danger btn-lg"
-            ><i class="fas fa-window-close"></i> CANCELAR</router-link>
+              <i class="fas fa-edit"></i>
+              ATUALIZAR
+            </button>
+            <router-link :to="{ name: 'listSponsors' }" tag="button" class="btn btn-outline-danger btn-lg">
+              <i class="fas fa-window-close"></i>
+              CANCELAR
+            </router-link>
           </form>
         </b-col>
         <b-col cols="2"></b-col>
       </b-row>
-     </b-container>
+    </b-container>
   </section>
 </template>
 
 <script>
 import { EDIT_SPONSOR } from "@/store/sponsors/sponsor.constants";
-import HeaderPage from "@/components/HeaderPage.vue"
+import HeaderPage from "@/components/HeaderPage.vue";
 import router from "@/router";
 import { mapGetters } from "vuex";
 
 export default {
   name: "EditSponsor",
   components: {
-    HeaderPage
+    HeaderPage,
   },
   data: () => {
     return {
-      sponsor: {}
+      sponsor: {},
     };
   },
   computed: {
-    ...mapGetters("sponsor", ["getSponsorById", "getMessage"])
+    ...mapGetters("sponsor", ["getSponsorById", "getMessage"]),
   },
   methods: {
     update() {
       this.$store.dispatch(`sponsor/${EDIT_SPONSOR}`, this.$data.sponsor).then(
         () => {
-          this.$alert(
-            this.getMessage,
-            "Sponsor atualizado!",
-            "success"
-          );
-          router.push({name: 'listSponsors'});
+          this.$alert(this.getMessage, "Sponsor atualizado!", "success");
+          router.push({ name: "listSponsors" });
         },
-        err => {
+        (err) => {
           this.$alert(`${err.message}`, "Erro", "error");
         }
       );
-    }
+    },
   },
   created() {
     this.sponsor = this.getSponsorById(this.$route.params.sponsorId);
-  }
+  },
 };
 </script>
